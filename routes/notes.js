cc89eac9e35f5665cbd0b120da9bf4e731f7e49f
treecao <94645628+treecao@users.noreply.github.com>
2023-03-03 20:19:55 -0600

    set routes to get, post, and push notes

--- routes/notes.js
+++ routes/notes.js
@@ -1,25 +1,30 @@
 //require things
 const notes = require('express').Router();
 const uuid = require('../helpers/uuid');
-const fsUtils = require('../helpers/fsUtils');
+const  { readFromFile, writeToFile, readAndAppend } = require('../helpers/fsUtils');
 const db = require('../db/db.json');
 
 //get routes using helper functions
 //route to get notes
-notes.get('/', (req,res) => {
-    console.info(`${req.method} request received for notes`);
-  readFromFile('../helpers/fsUtils.js').then((data) => res.json(JSON.parse(data)));
+notes.get('/notes', (req,res) => {
+    console.log(`${req.method} request received for notes`);
+  readFromFile('db/db.json', 'utf8').then((data) => res.json(JSON.parse(data)));
 })
 
-//route to post notes
-notes.post('/', (req,res) => {
-    console.info(`${req.method} request received to submit notes`);
-    const{} = req.body;
-})
-
-//route to delete notes
-notes.delete('/', (req,res) => {
-    
+notes.post('/notes', (req,res) => {
+  console.log(`${req.method} request received for notes`);
+  
+readFromFile('db/db.json', 'utf8').then((data) => {
+  //const notes = JSON.parse(data)
+  var notes = new Array();
+  notes.push({
+    ...req.body, 
+    id:uuid()
+  })
+  writeToFile('db/db.json', JSON.stringify(notes))
+  res.json(notes[notes.length-1])
+}
+);
 })
 
 module.exports = notes;
--- server.js
+++ server.js
@@ -9,9 +9,15 @@ const app = express();
 
 // Middleware for parsing JSON and urlencoded form data
 app.use(express.json());
-app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
 app.use('/api', api);
+
+// app.get('/api/notes', (req,res) => {
+//   console.log(`${req.method} request received for notes`);
+// // readFromFile('../db/db.json').then((data) => res.json({}));
+// res.json({});
+// })
 
 // GET Route for notes page 
 app.get('/notes', (req, res) =>
@@ -19,9 +25,9 @@ app.get('/notes', (req, res) =>
 );
 
 // GET Route for homepage 
-app.get('*', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/index.html'))
-);
+// app.get('*', (req, res) =>
+//   res.sendFile(path.join(__dirname, '/public/index.html'))
+// );
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
