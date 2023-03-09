5a3c25cdb7da21e4e8f0baa85b0410f0d8ae9962
Tree <94645628+treecao@users.noreply.github.com>
2023-03-08 23:27:31 -0600

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const path = require('path');
 const api = require('./routes/notes.js');
 
-const PORT = 3001;
+const PORT = process.env.PORT || 80;
 
 const app = express();
 
