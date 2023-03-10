af4e5465e636a803e09056853cda04ae284d0287
treecao <94645628+treecao@users.noreply.github.com>
2023-03-10 09:41:27 -0600

    update port

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const path = require('path');
 const api = require('./routes/notes.js');
 const htmlRoutes = require('./routes/htmlRoutes');
-const PORT = process.env.port || 3001;
+const PORT = process.env.PORT || 3001;
 
 const app = express();
 
