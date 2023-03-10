d4a07735e465b0d4d0940da257f2b8c381666942
treecao <94645628+treecao@users.noreply.github.com>
2023-03-09 22:44:09 -0600

    added htmlRoutes file; reroute

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const path = require('path');
 const api = require('./routes/notes.js');
-
+const htmlRoutes = require('./routes/htmlRoutes');
 const PORT = process.env.port || 3001;
 
 const app = express();
@@ -12,11 +12,15 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 app.use('/api', api);
-
+app.use('/', htmlRoutes)
 // GET Route for notes page 
-app.get('/notes', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/notes.html'))
-);
+// app.get('/notes', (req, res) =>
+//   res.sendFile(path.join(__dirname, './public/notes.html'))
+// );
+
+// router.get('/', (req, res) => {
+//   res.sendFile(path.join(__dirname, './public/index.html'));
+// });
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
