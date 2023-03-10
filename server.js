7b99fb6e163b161ea408c5e8f83bbb2af2c40936
treecao <94645628+treecao@users.noreply.github.com>
2023-03-09 22:16:30 -0600

    pushing to heroku

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const path = require('path');
 const api = require('./routes/notes.js');
 
-const PORT = 3001;
+const PORT = process.env.port || 3001;
 
 const app = express();
 
